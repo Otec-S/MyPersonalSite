0b406457872932633043ae61ba343f239a082757
Sergey Grigorash <adviser@bk.ru>
2024-10-19 12:30:55 +0500

    feat start nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,10 +9,22 @@ import {
 
 const Header: FC = () => {
   return (
-    <>
+    <header>
       <h1 className={styles.headerTitle}>Сергей Григораш</h1>
       <h2 className={styles.headerSubTitle}>Frontend Developer</h2>
       <p className={styles.text}>Сделаем это красиво</p>
+
+      <nav className={styles.navigation}>
+        <ul className={styles.navigationList}>
+          <li className={styles.navigationItem}>
+            <a href="#" className={styles.navigationLink}>
+              <span className={styles.navigationIcon}>-----</span>
+              <span className={styles.navigationText}>Обо мне</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
       <ul className={styles.socialMedia} aria-label="Social media">
         <li className={styles.socialMediaIcon}>
           <a
@@ -54,14 +66,14 @@ const Header: FC = () => {
             href="mailto:sergey.adviser@gmail.com"
             target="_blank"
             rel="noreferrer noopener"
-            aria-label="Telegram (opens in a new tab)"
-            title="Telegram"
+            aria-label="Email (opens in a new tab)"
+            title="Email"
           >
             <MailIcon />
           </a>
         </li>
       </ul>
-    </>
+    </header>
   );
 };
 
