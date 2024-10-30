9338a92ffdbadcdc6a7777578817465367d87936
Sergey Grigorash <adviser@bk.ru>
2024-10-30 21:27:25 +0500

    feat by clicking on Sergey Grigorash - go to the very start

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,37 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./header.module.css";
 
 import Navigation from "@components/navigation/navigation";
 import SocialMedia from "@components/social-media/social-media";
 
 const Header: FC = () => {
+  const [link, setLink] = useState<string>("#about");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1000) {
+        setLink("#");
+      } else {
+        setLink("#about");
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
       <div>
-        <h1 className={styles.headerTitle}>Сергей Григораш</h1>
+        <h1 className={styles.headerTitle}>
+          <a href={link} className={styles.headerTitleLink}>
+            Сергей Григораш
+          </a>
+        </h1>
         <h2 className={styles.headerSubTitle}>
           Frontend Developer | React TypeScript
         </h2>
