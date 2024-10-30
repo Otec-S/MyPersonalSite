44cf01e286ac7537150225b9f8a240669f30fc78
Sergey Grigorash <adviser@bk.ru>
2024-10-30 20:45:29 +0500

    feat finish responsive

--- src/components/gradient/gradient.tsx
+++ src/components/gradient/gradient.tsx
@@ -2,17 +2,30 @@ import { useState, useEffect } from "react";
 import styles from "./gradient.module.css";
 
 function Gradient() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 401 });
 
   useEffect(() => {
     const updateCursorPosition = (e: MouseEvent) => {
-      setPosition({ x: e.clientX, y: e.clientY });
+      if (window.innerWidth >= 1000) {
+        setPosition({ x: e.clientX, y: e.clientY });
+      } else {
+        setPosition({ x: 0, y: 401 });
+      }
     };
 
     window.addEventListener("mousemove", updateCursorPosition);
 
+    const handleResize = () => {
+      if (window.innerWidth < 1000) {
+        setPosition({ x: 0, y: 401 });
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
     return () => {
       window.removeEventListener("mousemove", updateCursorPosition);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
