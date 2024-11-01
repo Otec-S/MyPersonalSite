5e2dc935ac67a10f3e969aeee58b3f9a72f1b134
Sergey Grigorash <adviser@bk.ru>
2024-11-01 12:09:49 +0500

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,15 @@ import styles from "./App.module.css";
 import Main from "@components/main/main";
 import Gradient from "@components/gradient/gradient";
 import SmoothScroll from "@components/smooth-scroll/smooth-scroll";
-import OgImage from "@components/og-image/og-image";
 
 function App() {
   return (
-    <>
-      <div className={styles.app}>
-        <Header />
-        <SmoothScroll />
-        <Gradient />
-        <Main />
-      </div>
-      <OgImage />
-    </>
+    <div className={styles.app}>
+      <Header />
+      <SmoothScroll />
+      <Gradient />
+      <Main />
+    </div>
   );
 }
 
