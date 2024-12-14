08b7d8030ae33126289be36b27ebae7f9bf435dc
Sergey Grigorash <sergey.adviser@gmail.com>
2024-12-14 11:24:07 +0700

    feat make base i18n

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,15 @@ import styles from "./header.module.css";
 import Navigation from "@components/navigation/navigation";
 import SocialMedia from "@components/social-media/social-media";
 
+import { useTranslation, Trans } from "react-i18next";
+
+const lngs = {
+  en: { nativeName: "English" },
+  ru: { nativeName: "Russian" },
+};
+
 const Header: FC = () => {
+  const { t, i18n } = useTranslation();
   const [link, setLink] = useState<string>("#about");
 
   useEffect(() => {
@@ -27,15 +35,34 @@ const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div>
+        {Object.keys(lngs).map((lng) => (
+          <button
+            key={lng}
+            style={{
+              fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
+            }}
+            type="submit"
+            onClick={() => i18n.changeLanguage(lng)}
+          >
+            {lngs[lng].nativeName}
+          </button>
+        ))}
+      </div>
+      <div>
         <h1 className={styles.headerTitle}>
           <a href={link} className={styles.headerTitleLink}>
-            Сергей Григораш
+            {/* TODO: */}
+            <Trans i18nKey="description.part1">Сергей Григораш</Trans>
           </a>
         </h1>
         <h2 className={styles.headerSubTitle}>
           Frontend Developer | React TypeScript
         </h2>
-        <p className={styles.text}>Сделаю это красиво</p>
+        <p className={styles.text}>
+          {/* TODO: */}
+          {/* Сделаю это красиво */}
+          {t("description.part2")}
+        </p>
         <Navigation />
       </div>
       <SocialMedia />
