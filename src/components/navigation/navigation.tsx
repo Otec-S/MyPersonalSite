59245cba4b07b3a2cff1eda77c177211c0ad4386
Sergey Grigorash <sergey.adviser@gmail.com>
2024-12-22 09:52:37 +0700

    feat translate nav

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -2,15 +2,22 @@ import { FC } from "react";
 import styles from "./navigation.module.css";
 import NavigationItem from "./navigation-item/navigation-item";
 import useSectionVisibility from "@components/shared/hooks/useSectionVisibility";
+import { useTranslation } from "react-i18next";
 
 const Navigation: FC = () => {
   const { about, experience } = useSectionVisibility();
 
+  const { t } = useTranslation();
+
   return (
     <nav className={styles.navigation} aria-label="In-page jump links">
       <ul className={styles.navigationList}>
-        <NavigationItem link="#about" text="Обо мне" isActive={about} />
-        <NavigationItem link="#experience" text="Опыт" isActive={experience} />
+        <NavigationItem link="#about" text={t("nav.about")} isActive={about} />
+        <NavigationItem
+          link="#experience"
+          text={t("nav.experience")}
+          isActive={experience}
+        />
       </ul>
     </nav>
   );
